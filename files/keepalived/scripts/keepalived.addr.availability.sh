dac73bbfdde8ae98ba6a74218f4d3aefd5ba622d
PrimeRevJLawrie <jlawrie@primerevenue.com>
2015-11-09 16:56:27 -0500

    Update keepalived.addr.availability.sh

--- files/keepalived/scripts/keepalived.addr.availability.sh
+++ files/keepalived/scripts/keepalived.addr.availability.sh
@@ -5,7 +5,7 @@
 PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 IPADDR=$1
 
-[[ $1 ]] || { echo "ZBX_NOTSUPPORTED, need at least one parameter in which IP address must be specified."; exit 1; }
-[[ -f $(which ip 2>/dev/null) ]] || { echo "ZBX_NOTSUPPORTED, ip utility from iproute2 not found."; exit 1; }
+#[[ $1 ]] || { echo "ZBX_NOTSUPPORTED, need at least one parameter in which IP address must be specified."; exit 1; }
+#[[ -f $(which ip 2>/dev/null) ]] || { echo "ZBX_NOTSUPPORTED, ip utility from iproute2 not found."; exit 1; }
 
 if ip addr show |grep -qo $IPADDR; then echo "Up"; else echo "Not present"; fi
